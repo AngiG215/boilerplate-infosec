b6af4b44318567f4d5245532a29552585c03695e
Angela <angieadrigarcia572@gmail.com>
2026-02-15 21:58:30 -0300

    Refactor myApp.js to enhance security and simplify code
    Removed unused middleware and routes, and added Helmet for security.

--- myApp.js
+++ myApp.js
@@ -1,61 +1,18 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 
+app.use(helmet.hidePoweredBy());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Esto es para que no se cierre el servidor
+app.get("/", (req, res) => {
+  res.send("¡Servidor funcionando con Helmet!");
+});
 
 module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+
+// El servidor escuchando
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`Tu servidor está corriendo en el puerto ${port}`);
 });
